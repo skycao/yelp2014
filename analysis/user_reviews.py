8d2fa224f4ab6e9567a0ca5e1d67361d48e16d22
Sky Cao <cao_sky@yahoo.com>
2014-08-20 12:14:25 -0700

    started user reviews analysis

---
+++ analysis/user_reviews.py
@@ -0,0 +1,20 @@
+FILE_DIR = "../utilities/"
+
+import sys
+sys.path.insert(0, FILE_DIR)
+
+import parse
+import numpy
+import matplotlib.pyplot as plt
+
+user_file = "yelp_academic_dataset_user.json"
+
+def get_user_review_counts():
+    user_data = parse.parse_into_dicts(user_file);
+    user_reviews = [user["review_count"] for user in user_data]
+    return user_reviews
+
+def user_reviews_hist():
+    plt.hist(get_user_review_counts())
+
+
