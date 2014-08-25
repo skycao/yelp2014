0cb28490f67caf41b1ccd29ab2c19ee2ac83444d
Sky Cao <cao_sky@yahoo.com>
2014-08-25 01:37:37 -0700

    added functions to calculate the tf.idf of terms in documents

--- utilities/document.py
+++ utilities/document.py
@@ -1,4 +1,5 @@
 import string
+import math
 
 # constructs the set of all k-shingles for the given document
 # where k is shingle_length
@@ -37,5 +38,68 @@ def jaccard_distance(set1, set2):
         else:
             union += 1
     return 1 - float(intersection) / union
+
+# returns a dictionary where the keys are the terms appearing
+# in document, and the values are the corresponding term frequencies
+def term_freq(document):
+    assert type(document) is str, "must provide a str"
+
+    document = document.lower()
+    term_freq_dict = {}
+    terms = strip_punctuation(document).split()
+
+    max_freq = 0
+    for term in terms:
+        if term in term_freq_dict:
+            term_freq_dict[term] += 1
+        else:
+            term_freq_dict[term] = 1
+        if term_freq_dict[term] > max_freq:
+            max_freq = term_freq_dict[term]
+
+    if max_freq > 0:
+        for term in term_freq_dict:
+            term_freq_dict[term] /= max_freq
+            
+    return term_freq_dict
     
+# term_freq_dicts is a list of dictionaries, of the same format
+# as those returned by term_freq()
+# the definition of the IDF_i for a given term i is
+# log_2(N / n_i), where N is the total number of documents
+# and n_i is the number of documents where the term i occurs
+def inverse_doc_freq(term_freq_dicts):
+    assert type(term_freq_dicts) is list, "must provide a list (of dictionaries)"
+
+    inv_doc_freq_dict = {}
+    for freq_dict in term_freq_dicts:
+        for term in freq_dict:
+            if term in inv_doc_freq_dict:
+                inv_doc_freq_dict[term] += 1
+            else:
+                inv_doc_freq_dict[term] = 1
+
+    num_docs = len(term_freq_dicts)
+    for term in inv_doc_freq_dict:
+        inv_doc_freq_dict[term] = math.log(num_docs / float(inv_doc_freq_dict[term]), 2)
+    
+    return inv_doc_freq_dict
+
+# documents is a list of strings
+# returns a table of dictionaries
+# where each dictionary corresponds to a document
+# the keys are the terms in the document
+# the values are the TF.IDF values of the terms in the document
+def tf_idf(documents):
+    assert type(documents) is list, "must provide a list (of strings)"
+
+    term_freq_dicts = [term_freq(doc) for doc in documents]
+    inv_doc_freq_dict = inverse_doc_freq(term_freq_dicts)
+    
+    # for each table (corresponding to a document)
+    # create a dictionary with keys as the terms in the table
+    # and values as the TF.IDF values of the terms in the table
+    tf_idf_dict = [dict([(term, term_freq[term] * inv_doc_freq_dict[term]) for term in term_freq]) for term_freq in term_freq_dicts]
+
+    return tf_idf_dicts
     
