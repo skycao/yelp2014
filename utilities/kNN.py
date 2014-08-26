9c2da8c7671198b165635b636f67624ea40fae20
Sky Cao <cao_sky@yahoo.com>
2014-08-25 17:38:00 -0700

    added docstrings to kNN.py

--- utilities/kNN.py
+++ utilities/kNN.py
@@ -4,6 +4,14 @@
 import heapq
 
 def makeKNNClassifier(data, labels, k, distance):
+    """ Constructs a function that classifies data points by using
+    k-nearest neighbors. 
+
+    data -- the data points, should be a list
+    labels -- the labels of the corresponding data points, should be a list
+    k -- the number of neighbors to consider when classifying points
+    distance -- the distance metric, should be a function that takes 2 parameters
+    """
     def classify(x):
         closestPoints = heapq.nsmallest(k, enumerate(data),
                                         key=lambda y: distance(x, y[1]))
