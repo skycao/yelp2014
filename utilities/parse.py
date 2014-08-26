f8c70f2a76198d88637a234c27b77528720b0e86
Sky Cao <cao_sky@yahoo.com>
2014-08-25 17:35:43 -0700

    added docstrings to parse.py

--- utilities/parse.py
+++ utilities/parse.py
@@ -1,9 +1,13 @@
 import json
-from pprint import pprint
 
 FILE_DIR = "../data/"
 
 def parse_into_dicts(json_file, file_dir=FILE_DIR):
+    """ Parses a JSON-formatted file into a list of dictionary objects
+    
+    json_file -- the name of the file
+    file_dir -- the directory where the file resides
+    """
     json_data = open(file_dir + json_file)
     decoder = json.JSONDecoder()
     data = []
