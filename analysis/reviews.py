54041a140d18e54431d14f89fc15132b7a126508
Sky Cao <cao_sky@yahoo.com>
2014-08-28 00:04:27 -0700

    added script to compute TF.IDF values

--- analysis/reviews.py
+++ analysis/reviews.py
@@ -1,9 +1,13 @@
 # execute with python3 -i reviews.py run
+# run with python3 -i reviews.py run TIMED
+# to have certain functions time themselves
 
 from config import *
 from yelp2014.utilities import parse
 from yelp2014.utilities import document
 from yelp2014.utilities import kNN
+
+TIMED = False
 
 user_file = "yelp_academic_dataset_review.json"
 SHINGLE_LENGTH = 4
@@ -99,7 +103,11 @@ def train_review_data():
         data += category[:length]
         # reviews in each category have their star rating as the label
         labels += [rating + 1] * length
-    return kNN.makeKNNClassifier(data, labels, NEAREST_NEIGHBOR_THRESHOLD, review_distance)
+    classifer = kNN.makeKNNClassifier(data, labels, NEAREST_NEIGHBOR_THRESHOLD, review_distance)
+    if TIMED:
+        return timed(classifier)
+    else:
+        return classifier
     
 # Testing the data is too slow. The reason why is that the k-NN classifier is learned
 # on 500,000 points. When a new data point needs to be classified, a priority heap of
@@ -111,7 +119,6 @@ def test_review_data():
     observed at each test point. 
 
     """
-    classifier = train_review_data()
     print("learning done")
     data, labels = [], []
     for rating, category in enumerate(partition):
@@ -125,8 +132,33 @@ def test_review_data():
         abs_dist.append(abs(label - classifier(datum)))
     return abs_dist
 
+def compute_tf_idf(review_data):
+    """ Computes the tf_idf scores of the words found in the reviews
+    of review_data. Returns a list of dictionaries. Each dictionary corresponds
+    to a review, and maps words found in the review to the words' tf_idf scores.
+    
+    review_data -- a list of dictionaries, each containing a "text" field
+    """
+    reviews = [review["text"] for review in review_data]
+    return document.tf_idf(reviews)
+    
+    
+def construct_tf_idf_tables():
+    """ Computes the tf_idf scores for each of the star rating categories.
+    The individual categories are treated as different sets of documents, so
+    5 sets of tf_idf scores are computed. A list of five lists is returned, where
+    each nested list is a list of dictionaries. Each nested list corresponds to
+    a star rating category, and each dictionary in the nested list corresponds to
+    a review in that star rating category. The dictionaries maps words found in the
+    corresponding reviews to the words' tf_idf scores.
+
+    """
+    return [compute_tf_idf(category) for category in partition]
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and str(sys.argv[1]) == "run":
+    if "TIMED" in sys.argv:
+        TIMED = True
+    if "run" in sys.argv:
         partition = partition_by_rating()
         print("partition done")
         result = test_review_data()
